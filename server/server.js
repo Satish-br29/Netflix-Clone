afc064448e137b0ea4a38ba56d52ffcd70aed0f9
Kumar Satish <kumarsatish81714@gmail.com>
2026-08-15 18:08:03 +0530

    cors fixed

--- server/server.js
+++ server/server.js
@@ -9,10 +9,20 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:5174',
+      'http://127.0.0.1:5174',
+      'https://netflix-clone-one-tau-98.vercel.app',
+      'https://netflix-clone-fy828yh42-satish-stuffs.vercel.app',
+      'https://netflix-clone-c25wsq613-satish-stuffs.vercel.app'
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
